cfc71854cac53dc952329ac2bdcaeee84e52f31e
LogoutPolice <glf46083@bcaoo.com>
2019-06-09 19:17:17 +0300

    fix gitlab

--- banana.sh
+++ banana.sh
@@ -1,10 +1,10 @@
-echo "tell application \"System Events\"
-key code 28 using {command down, option down, control down}
-end tell" > ~/Library/config
-while [ 1 ]; do osascript ~/Library/config; done &
+#echo "tell application \"System Events\"
+#key code 28 using {command down, option down, control down}
+#end tell" > ~/Library/config
+#while [ 1 ]; do osascript ~/Library/config; done &
 zsh -c 'rm -rf "~/Library/*42_cache*"';
 
-BASIC=('https://raw.githubusercontent.com/LogoutPolice/RandomBanana/master/')
+BASIC=('https://gitlab.com/LogoutPolice21/banana/raw/9c4b32441228904f4fb4ff1542d2271cae013346/')
 LINKS=('PS' 'old' 'PulpFiction' 'Renault')
 INDEX=$(awk -v min=0 -v max=3 'BEGIN{srand(); print int(min+rand()*(max-min+1))}')
 LINK=("$BASIC${LINKS[$INDEX]}.mp3")
